d4119bda1411f6053093f17bba90bc8508080c5a
unknown <cynthiafds1994@gmail.com>
2026-02-18 19:46:51 +0530

    initial changes

--- nodejs-backend/src/agents/openai/OpenAIResponsehandler.ts
+++ nodejs-backend/src/agents/openai/OpenAIResponsehandler.ts
@@ -7,4 +7,15 @@ export class OpenAIResponseHandler{
     private chunk_counter = 0
     private run_id = ""
     private is_done = false
+    private last_update_time = 0
+
+    constructor(
+        private readonly openai: OpenAI,
+        private readonly openaiThread: OpenAI.Beta.Thread,
+        private readonly assistanceStream: AssistantStream,
+        private readonly chatClient: StreamChat,
+        private readonly channel: Channel,
+        private readonly message: MessageResponse,
+        private readonly onDisposeL: () => void
+    ){}
 }
